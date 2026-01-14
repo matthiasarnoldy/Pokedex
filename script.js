f0c83cb16e44d13c3e9f34dad5b1164ced788c37
Matthias Arnoldy <m.arnoldy@outlook.de>
2026-01-14 10:33:05 +0100

    feat: add loadData and renderPokemon name

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-const BASE_URL = "https://remotestorage-94f04-default-rtdb.europe-west1.firebasedatabase.app/";
+const BASE_URL = "https://pokeapi.co/api/v2/";
+const allPokemon = [];
+
+function init() {
+    loadData("pokemon?limit=25&offset=24")
+}
+
+async function loadData(path="") {
+    let response = await fetch(BASE_URL + path + ".json")
+    let responseAsJson = await response.json();
+    responseAsJson.results.forEach(pokemon => allPokemon.push(pokemon));
+    // console.log(responseAsJson);
+    // loadMoreData("pokemon?limit=25&offset=49") // path durch responseAsJson.next ersetzen?
+    pokemonUrlToPath();
+}
+
+function pokemonUrlToPath() {
+    allPokemon.forEach((pokemon) => {
+        pokemon.url = (pokemon.url.slice(26));
+    });
+    console.log(allPokemon)
+    renderPokemonCard();
+}
+
+function renderPokemonCard() {
+    let pokemonAreaRef = document.getElementById('pokemonArea');
+    pokemonAreaRef.innerHTML = '';
+    allPokemon.forEach((pokemon) => {
+        pokemonAreaRef.innerHTML += getPokemonCardTemplate(pokemon);
+    });
+}
+
+// async function loadMoreData(path="") {
+//     let response = await fetch(BASE_URL + path + ".json")
+//     let responseAsJson = await response.json();
+//     responseAsJson.results.forEach(pokemon => allPokemon.push(pokemon));
+//     console.log(allPokemon);
+// }
